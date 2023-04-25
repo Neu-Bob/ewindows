a794292e30c134d2f34e6ef422ee08eb2dc84df4
neubob <bob@neubaum.com>
2023-04-25 10:26:33 -0400

    Update functionNodefinal.js

--- functionNodefinal.js
+++ functionNodefinal.js
@@ -1,4 +1,4 @@
-//ultimategoal is to ramp up a light beginning at sunrise, climbing to 100% by noon then ramping down for the remainder of daylight
+//ultimate goal is to ramp up a light beginning at sunrise, climbing to 100% by noon then ramping down for the remainder of daylight
 
 
 //use sun node to create variables for each sun event - these variables should be Eastern Standard Time, without dates
@@ -8,9 +8,15 @@
 //create a variable and output a msg.dayLightMinutes for dayLightMinutes calculated by sunrise and sunset times
 //create a variable and out for daylightMinutesRemaining
 
-
+//Ramp Up & Ramp Down
 
 //create a variable called brightness_pct
+
+//ramp up brightness percentage 
+//dayLightMinutes - 
+
+
+//ramp down birghtness percentage
 //
 
 
