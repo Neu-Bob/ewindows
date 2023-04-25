20996c8caab69492ae45090df7f84d84c9f306d0
neubob <bob@neubaum.com>
2023-04-25 10:29:40 -0400

    Update functionNodefinal.js

--- functionNodefinal.js
+++ functionNodefinal.js
@@ -4,7 +4,6 @@
 //use sun node to create variables for each sun event - these variables should be Eastern Standard Time, without dates
 
 
-
 //create a variable and output a msg.dayLightMinutes for dayLightMinutes calculated by sunrise and sunset times
 //create a variable and out for daylightMinutesRemaining
 
@@ -21,8 +20,6 @@
 
 //ramp down birghtness percentage
 //
-
-
 
 
 return msg;
