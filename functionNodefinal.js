03434753725467d64feb8e8bee0397b43126dd07
neubob <bob@neubaum.com>
2023-04-25 11:21:21 -0400

    Update functionNodefinal.js

--- functionNodefinal.js
+++ functionNodefinal.js
@@ -2,12 +2,33 @@
 
 
 //use sun node to create variables for each sun event - these variables should be Eastern Standard Time, without dates
+const events = msg.payload.sunevents;
+const timeZoneEDT = 'America/New_York';
+let sunriseTime;
+let sunsetTime;
+
 events.forEach(event => {
-    let eventName = event.event_name;
-    let eventDateTime = new Date(event.datetime);
-    let localTimeEDT = new Intl.DateTimeFormat('en-US', { timeZone: 'America/New_York', hour: '2-digit', minute: '2-digit', second: '2-digit', fractionalSecondDigits: 3, hourCycle: 'h23' }).format(eventDateTime);
+    const eventName = event.event_name;
+    const eventDateTime = new Date(event.datetime);
+    const localTimeEDT = new Intl.DateTimeFormat('en-US', { timeZone: timeZoneEDT, hour: '2-digit', minute: '2-digit', second: '2-digit', fractionalSecondDigits: 3, hourCycle: 'h23' }).format(eventDateTime);
     msg[eventName] = { time: localTimeEDT };
+
+    if (eventName === 'sunrise') {
+        sunriseTime = localTimeEDT;
+    } else if (eventName === 'sunset') {
+        sunsetTime = localTimeEDT;
+    }
 });
+
+const sunrise = new Date('2000-01-01T' + sunriseTime + ':00.000-04:00');
+const sunset = new Date('2000-01-01T' + sunsetTime + ':00.000-04:00');
+const daylightMinutes = (sunset.getTime() - sunrise.getTime()) / (1000 * 60);
+
+msg.sunriseTime = sunriseTime;
+msg.sunsetTime = sunsetTime;
+msg.daylightMinutes = daylightMinutes;
+
+return msg;
 
 
 
@@ -27,6 +48,3 @@ events.forEach(event => {
 
 //ramp down birghtness percentage
 //
-
-
-return msg;
